1740218fe5cd6fdc7bc6f9ec331e86e00e8bfd53
rahma408 <61933946+rahma408@users.noreply.github.com>
2020-05-14 14:56:59 +0700

    Add files via upload

---
+++ Epson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_Interface
+{
+    public class Epson : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("\nEpson printer display dimension: 10*11");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Epson printer printing...");
+        }
+    }
+}
---
+++ Laserjet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism_Interface
+{
+    public class LaserJet : IPrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("\nLaserJet printer display dimension: 12*12");
+        }
+        public void Print()
+        {
+            Console.WriteLine("LaserJet printer printing...");
+        }
+    }
+}
